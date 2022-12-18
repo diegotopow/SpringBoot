56d9d1d8ebf9179d36ac78799a229f89e78ae357
Diego Lopes <82356386+diegotopow@users.noreply.github.com>
2022-12-18 17:18:28 -0300

    Testando Microserviços com SpringBoot

---
+++ src/main/java/br/com/springboot/springboot/model/User.java
@@ -0,0 +1,27 @@
+package br.com.springboot.springboot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String username;
+   
+}
